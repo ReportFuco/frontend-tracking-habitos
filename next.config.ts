ff94f11f105904efbca73d3974ec580d1bc6fd2d
Francisco Arancibia <frarancibia.g@gmail.com>
2026-03-23 10:36:35 -0300

    "Cambios en las bulnerabilidades"

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  allowedDevOrigins: ["100.107.250.78"],
+  allowedDevOrigins: ["100.107.250.78", "100.124.185.116"],
 };
 
 export default nextConfig;
